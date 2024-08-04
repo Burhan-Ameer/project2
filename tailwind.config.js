be0aae63d8e5f2aaa71475cd9dbfeff9859a5e8f
Burhan-Ameer <burhanameer2@gmail.com>
2024-08-04 06:17:42 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:"480px",
+      md:"764px",
+      lg:"986px",
+      xl:"1440px"
+    },
+    extend: {
+
+      fontFamily: {
+        edu:[' Edu AU VIC WA NT Hand',' cursive;'],
+      anton:["Anton SC"]
+      },
+
+animation:{
+
+'spin-slow':'spin  3s linear infinite'
+},
+     colors:{
+        crimson:"#FF5733"
+      },
+      screens:{
+'xs':{'min':'320px','max':'640px'}
+      }
+    },
+  },
+  plugins: [],
+}
+
